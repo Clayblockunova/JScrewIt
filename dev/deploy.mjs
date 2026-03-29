459ad0856f46e3abefa6330b47a3335cf0833f4a
Francesco Trotta <github@fasttime.org>
2026-03-29 23:05:00 +0200

    add compatibility notice banner generation to deploy tool

--- dev/deploy.mjs
+++ dev/deploy.mjs
@@ -4,6 +4,7 @@ import { constants as fsConstants, fstatSync, readFileSync }    from 'node:fs';
 import { access, readFile }                                     from 'node:fs/promises';
 import { dirname }                                              from 'node:path';
 import { Readable }                                             from 'node:stream';
+import { parseArgs }                                            from 'node:util';
 import { Client }                                               from 'basic-ftp';
 
 const JSCREWIT_MIN_PATH = 'lib/jscrewit.min.js';
@@ -17,12 +18,89 @@ const REMOTE_HOME = '/html';
 if (!await deploy())
     process.exitCode = 1;
 
-async function createIndex()
+async function createIndex(compatibilityNotice)
 {
     const input = await readFile('JScrewIt.html', 'utf-8');
     const { groups: { start, end } } = input.match(/^(?<start>.*)\n\n(?<end><body>.*)\n$/s);
+    const bannerStyleMarkup =
+    compatibilityNotice ? `
+<!-- Compatibility notice banner style element inserted -->
+<style>
+
+body.hasBanner { padding-top: 40px; }
+
+#compatibilityNotice
+{
+    align-items: center;
+    background: #b3d8ff;
+    border-bottom: thin solid #e0e0e0;
+    color: black;
+    display: none;
+    font: 13.5pt sans-serif;
+    height: 40px;
+    justify-content: space-between;
+    left: 0;
+    padding: 4px 16px;
+    position: fixed;
+    right: 0;
+    top: 0;
+    z-index: 1;
+}
+
+body.hasBanner #compatibilityNotice { display: flex; }
+
+#compatibilityNotice a { color: black; text-decoration: underline; }
+
+#compatibilityNotice button
+{
+    align-items: center;
+    background: transparent;
+    border: none;
+    color: black;
+    cursor: pointer;
+    display: flex;
+    font: 24px/24px sans-serif;
+    justify-content: center;
+    padding: 0;
+    width: 24px;
+}
+
+#compatibilityNotice button:hover { opacity: .5; }
+
+</style>
+<!-- End of insertion -->` : '';
+    const bannerHTML = compatibilityNotice ? `
+<!-- Compatibility notice banner HTML inserted -->
+<div id='compatibilityNotice'>
+    <span>
+    Compatibility with older engines has been removed.
+    <a href='https://github.com/fasttime/JScrewIt?tab=readme-ov-file#compatibility'
+    target='_blank'>Learn more…</a>
+    </span>
+    <button id='closeBannerButton'>×</button>
+</div>
+<script>
+addEventListener
+(
+    'DOMContentLoaded',
+    function ()
+    {
+        if (localStorage.getItem('bannerClosed'))
+            return;
+        var bodyClassList = document.body.classList;
+        bodyClassList.add('hasBanner');
+        closeBannerButton.onclick =
+        function ()
+        {
+            localStorage.setItem('bannerClosed', '1');
+            bodyClassList.remove('hasBanner');
+        };
+    }
+);
+</script>
+<!-- End of insertion -->` : '';
     const output =
-    `${start}
+    `${start}${bannerStyleMarkup}
 <!-- Fork me ribbon style element inserted -->
 <style>
 
@@ -45,6 +123,8 @@ async function createIndex()
     transition: font .25s;
     width: calc((60px + (6px + 3px)) * 2 * 1.414 + 36px);
 }
+
+body.hasBanner #forkmeongithub { top: calc(40px + 60px - 36px / 2); }
 
 @media screen and (min-width: 800px) { #forkmeongithub { display: block; } }
 
@@ -72,7 +152,7 @@ async function createIndex()
 ${end}
 <!-- Fork me ribbon HTML inserted -->
 <a id='forkmeongithub' href='https://github.com/fasttime/JScrewIt'>Fork me on GitHub</a>
-<!-- End of insertion -->
+<!-- End of insertion -->${bannerHTML}
 `;
 
     const readable = Readable.from([output]);
@@ -83,6 +163,15 @@ async function deploy()
 {
     if (!await verifyBuild())
         return false;
+    const { values: { 'compatibility-notice': compatibilityNotice, preview } } =
+    parseArgs
+    ({ options: { 'compatibility-notice': { type: 'boolean' }, preview: { type: 'boolean' } } });
+    const readable = await createIndex(compatibilityNotice);
+    if (preview)
+    {
+        readable.pipe(process.stdout);
+        return true;
+    }
     const ftpAccessOptions = readFTPAccessOptions();
     if (ftpAccessOptions == null)
         return false;
@@ -92,7 +181,6 @@ async function deploy()
         await client.access(ftpAccessOptions);
 
         await client.ensureDir(REMOTE_HOME);
-        const readable = await createIndex();
         await client.uploadFrom(readable, 'index.html');
 
         await client.ensureDir('lib');
